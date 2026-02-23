40fc6c4d7c33a111b1636a6def6ad75a822604fb
David Cook <dcook@divviup.org>
2026-02-23 16:39:11 -0600

    Add test using transcript with GF(2^128)

--- docs/specs/code/tests/test_fs.py
+++ docs/specs/code/tests/test_fs.py
@@ -1,6 +1,6 @@
 import unittest
 
-from fields import Fp256
+from fields import Fp256, GF2_128
 from fs import Transcript
 
 
@@ -46,3 +46,21 @@ class TestFiatShamir(unittest.TestCase):
         for cs in choose_sizes:
             gotc = t.generate_nats_wo_replacement(cs, 20)
             print(gotc)
+
+    def test_gf2_128(self):
+        field = GF2_128
+        t = Transcript()
+        t.init(b"transcript binary field test")
+
+        elem = t.generate_field(field)
+        assert elem in field
+        assert elem != field.zero()
+        assert elem != field.one()
+
+        t.write_field(elem)
+
+        t.write_field_element_array([
+            field.from_integer(10),
+            field.from_integer(20),
+            field.from_integer(30),
+        ])
